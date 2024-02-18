00ce7bab796d05eb0bc90e157c827c4c4cbc4248
Selva Irudayam <selvaxavier.rajam@gmail.com>
2024-02-18 02:03:48 -0600

    javaFile1 created

---
+++ pgm.java
@@ -0,0 +1,11 @@
+public class pgm{
+public static void main(String[] args){
+    printFn();
+
+}
+    public static void printFn(){
+        System.out.println("Welcome to Java World");
+    }
+
+
+}
